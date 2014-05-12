c466b9fa54337421f3342642a8e28ee8513dcc0d
lucaslg26 <lucaslg26@yahoo.com.br>
2014-05-12 09:05:06 -0300

    Update inc.mysql.connect.php

--- inc/inc.mysql.connect.php
+++ inc/inc.mysql.connect.php
@@ -1,11 +1,5 @@
 <?php
-/* PLUG.GED 0.1 
-Sistema de gerenciamento para plug.dj.
-Criado por Lucas Vasconcelos(@lucaslg26)
-Com a ajuda de Luiz Eduardo(@Caipira).
-*/
-
-include_once "inc/inc.db.php";
+e_once "inc/inc.db.php";
 
 $a=mysql_connect($host,$user,$pass);
 	if (!$a){
